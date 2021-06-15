90ffe8c506cd97976236c51019eabf13b7e85ac0
LuisCarlos-git <luisestrela.bol951@gmail.com>
2021-06-15 14:53:42 -0300

    ajust parameters stories

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -5,6 +5,11 @@ import Button, { ButtonProps } from '.';
 export default {
   title: 'Button',
   component: Button,
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
+  },
 } as Meta;
 
 export const ButtonDefault: Story<ButtonProps> = args => <Button {...args} />;
