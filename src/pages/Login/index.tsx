fce9c4cccf41456010a2e601907cb5fb1327f890
LuisCarlos-git <luisestrela.bol951@gmail.com>
2021-06-15 10:53:33 -0300

    update

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Layout } from '../../components';
+import Layout from '../../components/Layout';
 
-// import * as Styled from './styles';
+import * as Styled from './styles';
 
 export default function Login() {
   return (
     <Layout>
-      <h1>HEllO MARVEl</h1>
+      <Styled.Wrapper>
+        <h1>test</h1>
+      </Styled.Wrapper>
     </Layout>
   );
 }
