a8dae13a72cf418d2ac43f19e3c659bfd892da85
LuisCarlos-git <luisestrela.bol951@gmail.com>
2021-06-16 15:04:34 -0300

    create page login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+
 import Layout from '../../components/Layout';
+import FormLogin from '../../components/FormLogin';
+
+import image from '../../assets/IronManImage.svg';
 
 import * as Styled from './styles';
 
@@ -7,7 +11,10 @@ export default function Login() {
   return (
     <Layout>
       <Styled.Wrapper>
-        <h1>test</h1>
+        <FormLogin />
+        <Styled.Image>
+          <img src={image} alt="" />
+        </Styled.Image>
       </Styled.Wrapper>
     </Layout>
   );
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,3 +1,17 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  max-width: 192rem;
+  margin: 0 auto;
+
+  align-items: center;
+`;
+export const Image = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  img {
+    height: 100vh;
+  }
+`;
