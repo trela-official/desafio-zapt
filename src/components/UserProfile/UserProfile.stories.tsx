f2c4b73f560461c6ebcbda6695e193bcc00bca4a
LuisCarlos-git <luisestrela.bol951@gmail.com>
2021-07-17 10:28:18 -0300

    add user profile stories

---
+++ src/components/UserProfile/UserProfile.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Story, Meta } from '@storybook/react/types-6-0';
+import UserProfile, { UserProfileProps } from '.';
+
+export default {
+  title: 'UserProfile',
+  component: UserProfile,
+} as Meta;
+
+export const UserProfileDefault: Story<UserProfileProps> = args => (
+  <UserProfile {...args} />
+);
+UserProfileDefault.args = {
+  username: '@LuisCarlos',
+  imageProfile:
+    'https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg',
+};
