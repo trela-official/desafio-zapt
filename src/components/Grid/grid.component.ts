0d83b4ca656b0d513b61ee1f6710de98bd797650
RiczCalixto <rcalixtos@gmail.com>
2021-07-17 16:41:39 -0300

    feat: add grid component

---
+++ src/components/Grid/grid.component.ts
@@ -0,0 +1,9 @@
+import {
+  Col as FlexCol,
+  Grid as FlexGrid,
+  Row as FlexRow
+} from 'react-styled-flexboxgrid'
+
+export const Grid = FlexGrid
+export const Col = FlexCol
+export const Row = FlexRow
