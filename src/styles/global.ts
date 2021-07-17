ef6efd56fcc1c00c0ce914ac20dcaa6356c5f33e
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-16 22:23:12 -0300

    add fonts to global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,31 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 300;
+    src: local(''),
+        url('../fonts/inter-v3-latin-300.woff2') format('woff2')
+  }
+
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    src: local(''),
+        url('../fonts/inter-v3-latin-regular.woff2') format('woff2')
+  }
+
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    src: local(''),
+        url('../fonts/inter-v3-latin-500.woff2') format('woff2')
+  }
+
   * {
     margin: 0;
     padding: 0;
