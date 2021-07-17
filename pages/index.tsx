42fdc0ea1c0d8dbfaaf08f3a60207b6ed9c4023f
Polyane Tuag <pollyportugues@hotmail.com>
2021-07-17 18:20:10 -0300

    home page structure

--- pages/index.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function Home() {
-  return (<h1>testßß</h1>)
-
-
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import Home from "../templates/Home"
+
+export default function index() {
+  return (<Home />)
+}
