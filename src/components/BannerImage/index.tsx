1a81efda5bb58cd177111eca586b4ae3325dafc2
Caio Grossi <caiogrossi6@gmail.com>
2021-07-17 09:42:02 -0300

    Added BannerImage component

---
+++ src/components/BannerImage/index.tsx
@@ -0,0 +1,7 @@
+import * as S from "./styles";
+
+const BannerImage = () => {
+  return <S.Wrapper></S.Wrapper>;
+};
+
+export default BannerImage;
---
+++ src/components/BannerImage/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+import bgImg from "assets/images/welcome.png";
+
+export const Wrapper = styled.div`
+  height: 100%;
+  width: 55rem;
+  background-image: url(${bgImg});
+`;
