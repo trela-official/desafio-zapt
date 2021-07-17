fc8789daa40557395da622e1ce9c1e211abcfce2
Polyane Tuag <pollyportugues@hotmail.com>
2021-07-17 18:19:34 -0300

    adding theme styles

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,58 +1,58 @@
 export default {
   grid: {
-    container: "130rem",
-    gutter: "3.2rem",
+    container: '130rem',
+    gutter: '2.4rem'
   },
   border: {
-    radius: "0.4rem",
+    radius: '0.4rem',
+    none: 'none'
   },
   font: {
     family: {
       primary:
         "Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
       secondary:
-        "PlusJakarta, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+        "PlusJakarta, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
     },
 
-    light: 300,
     normal: 400,
-    bold: 600,
+    semibold: 500,
     sizes: {
-      xsmall: "1.2rem",
-      small: "1.4rem",
-      medium: "1.6rem",
-      large: "1.8rem",
-      xlarge: "2.0rem",
-      xxlarge: "2.8rem",
-      huge: "5.2rem",
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '2rem',
+      xlarge: '2.8rem',
+      xxlarge: '4.0rem'
     },
+    line: {
+      medium: '1.5',
+      large: '1.3'
+    }
   },
   colors: {
-    primary: "#334BC8",
-    secondary: "#FF5B58",
-    white: "#FFFFFF",
-    black: "#000000",
-    gray: "#7B7B7B",
-    darkBlue: "#001C33",
+    primary: '#334BC8',
+    secondary: '#FF5B58',
+    bgBanner: '#FAFAFA',
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#7B7B7B',
+    darkBlue: '#001C33'
   },
   spacings: {
-    xxsmall: "0.8rem",
-    xsmall: "1.6rem",
-    small: "2.4rem",
-    medium: "3.2rem",
-    large: "4.0rem",
-    xlarge: "4.8rem",
-    xxlarge: "5.6rem",
+    xxsmall: '0.8rem',
+    xsmall: '1.2rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
   },
   layers: {
     base: 10,
-    menu: 20,
-    overlay: 30,
-    modal: 40,
-    alwaysOnTop: 50,
+    overlay: 20
   },
   transition: {
-    default: "0.3s ease-in-out",
-    fast: "0.1s ease-in-out",
-  },
-} as const;
+    default: '0.3s ease-in-out'
+  }
+} as const
