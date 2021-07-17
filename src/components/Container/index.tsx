9e2d8a665a0c4e6c9156d35dccac9becc742758a
Caio Grossi <caiogrossi6@gmail.com>
2021-07-17 19:14:21 -0300

    Added Container component

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 120rem;
+  margin: auto;
+  margin-top: 6rem;
+  margin-bottom: 6rem;
+
+  > h1 {
+    font-size: 4rem;
+    text-align: center;
+    margin-bottom: 4.8rem;
+    font-weight: 300;
+  }
+`;
