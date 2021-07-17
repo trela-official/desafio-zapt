f1cee03f60e30118bd7f0fccf0a25f55308c3ab0
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-16 22:24:17 -0300

    setup storybook with theme provider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,33 @@
 import { RouterContext } from 'next/dist/next-server/lib/router-context'
+import { ThemeProvider } from 'styled-components'
+
 import GlobalStyles from '../src/styles/global'
+import theme from '../src/styles/theme'
 
 export const parameters = {
   nextRouter: {
     Provider: RouterContext.Provider
+  },
+  backgrounds: {
+    default: 'zapt-light',
+    values: [
+      {
+        name: 'zapt-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'zapt-dark',
+        value: theme.colors.darkBlue
+      }
+    ]
   }
 }
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
