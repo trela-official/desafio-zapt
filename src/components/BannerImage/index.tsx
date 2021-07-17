3bb17621eb9f0a193cbffbae323adbed188922d2
Caio Grossi <caiogrossi6@gmail.com>
2021-07-17 18:17:47 -0300

    Added Circle and ArcCircle to BannerImg

--- src/components/BannerImage/index.tsx
+++ src/components/BannerImage/index.tsx
@@ -1,7 +1,15 @@
+import arcCircle from "assets/images/arc_circle.svg";
+import bgImg from "assets/images/welcome.png";
 import * as S from "./styles";
 
 const BannerImage = () => {
-  return <S.Wrapper></S.Wrapper>;
+  return (
+    <S.Wrapper>
+      <S.BackgroundImg src={bgImg} />
+      <S.Circle />
+      <S.ArcCircle src={arcCircle} />
+    </S.Wrapper>
+  );
 };
 
 export default BannerImage;
--- src/components/BannerImage/styles.ts
+++ src/components/BannerImage/styles.ts
@@ -1,9 +1,28 @@
 import styled from "styled-components";
 
-import bgImg from "assets/images/welcome.png";
-
 export const Wrapper = styled.div`
+  position: relative;
   height: 100%;
-  width: 55rem;
-  background-image: url(${bgImg});
+  width: 60rem;
+`;
+
+export const BackgroundImg = styled.img`
+  height: 100%;
+  width: 100%;
+`;
+
+export const Circle = styled.div`
+  top: 2.6rem;
+  left: 8.8rem;
+  position: absolute;
+  border-radius: 50%;
+  background-color: white;
+  width: 8rem;
+  height: 8rem;
+`;
+
+export const ArcCircle = styled.img`
+  position: absolute;
+  top: 16rem;
+  left: 22.1rem;
 `;
