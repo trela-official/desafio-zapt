d6697e3cf17d3aeac9588b624e1f0769226bec27
RiczCalixto <rcalixtos@gmail.com>
2021-07-17 11:33:31 -0300

    core: boostrap project based on boilerplate

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Desafio Zapt</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
+        <meta
+          name="description"
+          content="Projeto feito para avaliar as habilidades com html, css e javascript"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
