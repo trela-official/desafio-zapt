2ca4f4c25f581d9cea5bd7f2eda95de40815f624
Luís Fernando Anizello <anizello@gmail.com>
2021-07-17 08:19:21 -0300

    Add tests helper

---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from 'react'
+import { render, RenderOptions } from '@testing-library/react'
+
+import { ThemeProvider } from 'styled-components'
+import theme from 'styles/theme'
+
+const customRender = (
+  ui: ReactElement,
+  { ...renderOptions }: RenderOptions = {}
+) => render(<ThemeProvider theme={theme}>{ui}</ThemeProvider>, renderOptions)
+
+export * from '@testing-library/react'
+export { customRender as render }
