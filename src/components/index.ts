b3db81d65fadb217b164b43554a7a0ab12b534d9
LuisCarlos-git <luisestrela.bol951@gmail.com>
2021-07-16 21:06:19 -0300

    ajust exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,2 @@
 export { default as Heading } from './TextComponents/Heading';
 export { default as Typography } from './TextComponents/Typography';
-export { default as WelcomeCard } from './WelcomeCard';
