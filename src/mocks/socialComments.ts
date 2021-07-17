6eb43d0ee0207ef53963a35934de9a94b1aa7cd7
LuisCarlos-git <luisestrela.bol951@gmail.com>
2021-07-17 14:46:26 -0300

    social mock

---
+++ src/mocks/socialComments.ts
@@ -0,0 +1,47 @@
+export default [
+  {
+    username: '@ferri-gui',
+    comment:
+      '”Não da para parar de comer.. esse final de semana pedi 20 pasteis da Ilma e acabaram TODOS em um só dia... O pessoal aqui de casa ficou viciado de um nível que terei que comprar toda semana... Assim não tem academia que secura rsrsrsrs Obrigado Zapt.”',
+  },
+  {
+    username: '@paolaC',
+    comment:
+      '”Estava precisando achar algum lugar que vendia produtos que vêem direto da fazenda... a Zapt salvou.. e ainda paquei um preço muito mais barato que supermecado 🙌”',
+  },
+  {
+    username: '@Galvão',
+    comment:
+      '”Todos os vinhos da vinícula Bueno estão disponíveis no catalogo da Zapt a um preço incrivél... não percam essa oportunidade...”',
+  },
+  {
+    username: '@LiciaR.',
+    comment:
+      '”Nunca tinha achado os produtos na Lush depois que ela saiu do Brasil.. só a Zapt para conseguir importalos a preço mais barato do que vendiam aqui! Muito obrigado, recomendo sempre a todos os amigos.”',
+  },
+  {
+    username: '@shitz',
+    comment:
+      '”Ontem chegou uma pizza que comprei na Zapt e me lembrou as que minha avó fazia... sensacional!”',
+  },
+  {
+    username: '@rafael',
+    comment:
+      '”Nossa cada dia tem uma oferta mais legal do que a outra.. Além de produtos de qualidade o préco é imbatível”',
+  },
+  {
+    username: '@gust',
+    comment:
+      '”Achei uma promoção incrível de roupinha de bebê, foi o que salvou o enxoval do Pedro.”',
+  },
+  {
+    username: '@Cookie',
+    comment:
+      '”Meu dono finalmente conseguiu achar uma comida que eu gosto! Agora vou conseguir engordar e ser feliz.”',
+  },
+  {
+    username: '@duque',
+    comment:
+      '“Não conhecia a Zapt.. um amigo meu indicou... falou para eu dar uma olhada nos preços das cervejas artesanais.. incrível.”',
+  },
+];
