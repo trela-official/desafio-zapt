8b27a1ba4c1c842a66c1dc4bbad929c36c5dadf4
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-16 22:23:39 -0300

    create theme provider styles

---
+++ src/styles/theme.ts
@@ -0,0 +1,41 @@
+export default {
+  border: {
+    radius: '0.4rem'
+  },
+  font: {
+    family:
+      "Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    weight: {
+      light: 300,
+      regular: 400,
+      medium: 500
+    },
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '2rem',
+      xlarge: '2.8rem',
+      huge: '4rem'
+    },
+    spacings: {
+      xxsmall: '0.8rem',
+      xsmall: '1.2rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '4rem',
+      xlarge: '4.8rem',
+      huge: '8rem'
+    },
+    colors: {
+      primary: '#334BC8',
+      secondary: '#FF5B58',
+      white: '#FAFAFA',
+      gray: '#7B7B7B',
+      lightBlue: '#2196F3',
+      neutralBlue: '#011FBB',
+      darkBlue: '#001C33',
+      black: '#000000'
+    }
+  }
+} as const
