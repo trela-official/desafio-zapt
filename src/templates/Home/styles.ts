d8f794b0784275f3cedd708aa91bf7445252fc3c
Polyane Tuag <pollyportugues@hotmail.com>
2021-07-18 10:45:55 -0300

    modifying home styles

--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -6,8 +6,7 @@ export const Container = styled.section`
     max-width: ${theme.grid.container};
     margin-left: auto;
     margin-right: auto;
-    padding-left: calc(${theme.grid.gutter} / 2);
-    padding-right: calc(${theme.grid.gutter} / 2);
+
     margin-top: 30px;
   `}
 `
