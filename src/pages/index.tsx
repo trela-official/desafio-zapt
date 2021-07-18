cf01d2e3c78034213a3f21802940e0499065209e
Éverton Toffanetto <evertondgn@hotmail.com>
2021-07-18 20:35:14 -0300

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,51 @@
 import * as C from 'components'
 import * as S from 'styles'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <S.Container>
       <C.HeadPage title="Página Inicial" />
+
+      <C.TitleSection title="Desafio Zapt" />
+
+      <S.BtnGroup>
+        <Link href="/boas-vindas" passHref>
+          <S.LinkBtn target="_blank" rel="noreferrer">
+            <C.Button
+              fullWidth={true}
+              color="blue"
+              size="large"
+              text="Página Welcome"
+              className="btn1"
+            />
+          </S.LinkBtn>
+        </Link>
+
+        <Link href="/comentarios_de_usuarios" passHref>
+          <S.LinkBtn target="_blank" rel="noreferrer">
+            <C.Button
+              fullWidth={true}
+              color="blue"
+              size="large"
+              text="Página UsersComments"
+              className="btn2"
+            />
+          </S.LinkBtn>
+        </Link>
+
+        <Link href="/comentarios_nas_redes_sociais" passHref>
+          <S.LinkBtn target="_blank" rel="noreferrer">
+            <C.Button
+              fullWidth={true}
+              color="blue"
+              size="large"
+              text="Página SocialComments"
+              className="btn3"
+            />
+          </S.LinkBtn>
+        </Link>
+      </S.BtnGroup>
     </S.Container>
   )
 }
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,3 +1,45 @@
-import styled from 'styled-components'
+import Layouts from 'layouts'
+import styled, { css } from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled(Layouts.Col)`
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 35rem;
+
+  ${({ theme }) => theme.breakPoint(800)} {
+    margin: 12rem auto;
+    padding: 0 2rem;
+  }
+`
+
+export const BtnGroup = styled(Layouts.Col)`
+  ${({ theme }) => css`
+    gap: ${theme.grid.gutter};
+    margin-top: ${theme.spacings.xtreme};
+    padding: 0 ${theme.spacings.large};
+
+    .btn1 {
+      opacity: 0;
+      animation: ${theme.animations.fadeInMove('0', '30px', '0')} 0.3s linear
+        forwards 0.2s;
+    }
+
+    .btn2 {
+      opacity: 0;
+      animation: ${theme.animations.fadeInMove('0', '30px', '0')} 0.3s linear
+        forwards 0.4s;
+    }
+
+    .btn3 {
+      opacity: 0;
+      animation: ${theme.animations.fadeInMove('0', '30px', '0')} 0.3s linear
+        forwards 0.6s;
+    }
+  `}
+  align-items: center;
+`
+
+export const LinkBtn = styled.a`
+  display: flex;
+  width: 100%;
+`
