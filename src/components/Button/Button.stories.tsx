0eaf7ec1944db9f5793a2d57020afe7e074c6bfe
LuisCarlos-git <luisestrela.bol951@gmail.com>
2021-07-18 14:20:19 -0300

    add setup test

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -9,3 +9,7 @@ export default {
 } as Meta;
 
 export const ButtonDefault: Story<ButtonProps> = args => <Button {...args} />;
+
+ButtonDefault.args = {
+  label: 'Criar um grupo',
+};
---
+++ src/components/Button/button.spec.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from '../../helpers/renderWithThame';
+
+import Button from '.';
+
+describe('<Button />', () => {
+  it('should render button with label', () => {
+    renderWithTheme(<Button label="button label" />);
+
+    expect(screen.getByText(/button label/i)).toBeInTheDocument();
+    expect(screen.getByText(/button label/i)).toMatchSnapshot();
+  });
+});
