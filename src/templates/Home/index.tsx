2a9c017cefa05432aeea0f59250db9196d2aeab8
Polyane Tuag <pollyportugues@hotmail.com>
2021-07-18 16:22:55 -0300

    adding banner components and testmonials to the home

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,14 +1,22 @@
 import Banner, { BannerProps } from '../../components/Banner'
+import Testmonials, { TestmonialsProps } from '../../components/Testmonials'
 
 import * as S from './styles'
 
 export type HomeTemplateProps = {
   banner: BannerProps
+  testmonials: TestmonialsProps
 }
 
-const Home = ({ banner }: HomeTemplateProps) => (
+const Home = ({ banner, testmonials }: HomeTemplateProps) => (
   <S.Container>
-    <Banner {...banner} />
+    <S.BannerWrapper>
+      <Banner {...banner} />
+    </S.BannerWrapper>
+
+    <S.TestmonialsWrapper>
+      <Testmonials testmonials={testmonials} />
+    </S.TestmonialsWrapper>
   </S.Container>
 )
 
