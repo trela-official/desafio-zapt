5de9a158818ac03ec0c11f2615797c5fe2dc3d8c
Alan Gabriel <contatoalangabriel@gmail.com>
2021-07-18 13:07:32 -0300

    fix: storybook next/image

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,7 +1,20 @@
+const webpack = require('webpack')
+
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials'],
   webpackFinal: (config) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.__NEXT_IMAGE_OPTS': JSON.stringify({
+          deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+          imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+          domains: [],
+          path: '/',
+          loader: 'default'
+        })
+      })
+    )
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
   }
--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -2,6 +2,15 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/GlobalStyles'
 import theme from 'styles/theme'
 
+import * as nextImage from 'next/image'
+
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: (props) => {
+    return <img {...props} />
+  }
+})
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
