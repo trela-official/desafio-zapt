d50b2ae63e7a262a2c62b17a0204f4b5462f0904
Polyane Tuag <pollyportugues@hotmail.com>
2021-07-18 11:03:54 -0300

    adding link manifest and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,26 @@
-import { AppProps } from "next/app";
-import GlobalStyles from '../styles/global';
-import theme from '../styles/theme';
-import { ThemeProvider } from 'styled-components';
-import Head from 'next/head';
+import { AppProps } from 'next/app'
+import GlobalStyles from '../styles/global'
+import theme from '../styles/theme'
+import { ThemeProvider } from 'styled-components'
+import Head from 'next/head'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-        <Head>
-          <title>Desafio Zapt</title>
-          <meta
-            name="description"
-            content="Desafio para vaga de Fron-End by Poly Tuag"
-          />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </ThemeProvider>
-  )
+      <Head>
+        <title>Desafio Zapt Polyane</title>
+        <link rel="shortcut icon" href="/assets/icon-384.png" />
+        <link rel="manifest" href="/manifest.json" />
 
+        <meta
+          name="description"
+          content="Desafio para vaga de Fron-End by Poly Tuag"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
