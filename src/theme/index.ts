57ed6c01e2bb8cf245e5415e70726aca6f35f803
Éverton Toffanetto <evertondgn@hotmail.com>
2021-07-18 20:35:54 -0300

    feat: update theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -11,14 +11,14 @@ export const T = {
   },
   font: {
     family:
-      "'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+      "'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
     normal: 400,
     bold: 500,
     sizes: {
       xSmall: '1.2rem',
       small: '1.4rem',
       medium: '1.6rem',
-      Large: '2.0rem',
+      Large: '2rem',
       xLarge: '2.8rem',
       xxLarge: '4rem'
     }
@@ -30,9 +30,11 @@ export const T = {
     gray: '#7A7A7A',
     grayHover: '#aaaaaa',
     grayLight: '#FAFAFA',
+    bullet: '#EBEDFA',
     white: '#fff',
     black: '#000',
     blue: '#011FBB',
+    blueDark: '#001C33',
     blueLight: '#E9F4FE',
     disabled: '#a1a4b0'
   },
@@ -44,23 +46,36 @@ export const T = {
     large: '2rem',
     xLarge: '2.4rem',
     xxLarge: '2.8rem',
-    xxxLarge: '3.2rem'
+    xxxLarge: '3.2rem',
+    xtreme: '4rem'
   },
   layers: {
     base: 10
   },
-  transitions: {
-    default: '0.3s ease-in-out'
-  },
   animations: {
     rotate: keyframes`
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
-  `
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    `,
+    fadeInMove: (x: string, y: string, z: string) => keyframes`
+      0% {
+        opacity: 0;
+        transform: translate3d(${x}, ${y}, ${z});
+      }
+      100% {
+        opacity: 1;
+        transform: translate3d(0, 0, 0);
+      }
+    `,
+    fadeIn: keyframes`
+      to {
+        opacity: 1;
+      }
+    `
   },
-  breakPoint: (num1: number) => `@media screen and (min-width: ${num1}rem)`
+  breakPoint: (width: number) => `@media screen and (min-width: ${width}px)`
 } as const
