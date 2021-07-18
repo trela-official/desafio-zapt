deee24a2e719d904e41a2b09a6258297321e7e61
Éverton Toffanetto <evertondgn@hotmail.com>
2021-07-18 20:37:50 -0300

    build: remove lines

--- next.config.js
+++ next.config.js
@@ -7,9 +7,6 @@ module.exports = withPWA({
   },
   optimizeFonts: false,
   images: {
-    deviceSizes: [320, 420, 768, 1024, 1200, 1300, 1500, 1700],
-    iconSizes: [],
-    domains: [],
     path: '/_next/image',
     loader: 'default'
   },
