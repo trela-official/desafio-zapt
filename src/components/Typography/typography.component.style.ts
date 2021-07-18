ac3400a0158d9bccc97011198949dd76cab47874
RiczCalixto <rcalixtos@gmail.com>
2021-07-17 21:42:10 -0300

    refactor: adjust font size

--- src/components/Typography/typography.component.style.ts
+++ src/components/Typography/typography.component.style.ts
@@ -4,7 +4,7 @@ import styled, { css } from 'styled-components'
 export const H2 = styled.h2`
   ${({ theme }) => css`
     color: ${theme.colors.black};
-    font-size: ${theme.font.sizes.xlarge};
+    font-size: ${theme.font.sizes.xxlarge};
     font-family: ${theme.font.family};
     font-weight: ${theme.font.normal};
     margin-bottom: ${theme.spacings.medium};
