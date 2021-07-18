3078509e56302de1c4ef3b82364acaa7bed04efa
Guillermo Angulo <anyermo@gmail.com>
2021-07-18 19:55:31 -0300

    Add mock for Welcome component

---
+++ src/components/Welcome/mock.ts
@@ -0,0 +1,11 @@
+export default {
+  username: 'Marcio',
+  message:
+    'Para você aproveitar nossas oportunidades primeiro você precisa estar dentro de um grupo, estamos aqui para lhe ajudar. ',
+  question: 'O que você prefere?',
+  options: [
+    { text: 'Criar um grupo', link: '/criar' },
+    { text: 'Entrar em um grupo', link: '/entrar' }
+  ],
+  contactNumber: 999999999
+}
