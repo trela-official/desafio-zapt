e79fc8062fcee311a405b6c0424b95de682cca3b
Éverton Toffanetto <evertondgn@hotmail.com>
2021-07-18 16:24:19 -0300

    feat: add mocks to api

---
+++ src/components/CardUsersComments/mock.ts
@@ -0,0 +1,62 @@
+export const dataUsersComments = [
+  {
+    userName: 'Patricia Godoy',
+    city: 'São Paulo',
+    state: 'São Paulo',
+    urlUserBg: 'slide-2.jpg',
+    urlAvatar: 'avatar-woman.jpg',
+    urlProfile: '/comentarios_de_usuarios',
+    comment:
+      '”Não consigo mais viver sem os produtos do Sítio, a diferença do que eu comprava antes e agora é abissal”'
+  },
+  {
+    userName: 'Marcinho Ribeiro',
+    city: 'Belo Horizonte',
+    state: 'Minas Gerais',
+    urlUserBg: 'slide-1.jpg',
+    urlAvatar: 'avatar-man.jpg',
+    urlProfile: '/comentarios_de_usuarios',
+    comment:
+      '”Depois que você começa a usar produtos de qualidade é bem difícil voltar... ”'
+  },
+  {
+    userName: 'Patricia Godoy',
+    city: 'São Paulo',
+    state: 'São Paulo',
+    urlUserBg: 'slide-2.jpg',
+    urlAvatar: 'avatar-woman.jpg',
+    urlProfile: '/comentarios_de_usuarios',
+    comment:
+      '”Não consigo mais viver sem os produtos do Sítio, a diferença do que eu comprava antes e agora é abissal”'
+  },
+  {
+    userName: 'Marcinho Ribeiro',
+    city: 'Belo Horizonte',
+    state: 'Minas Gerais',
+    urlUserBg: 'slide-1.jpg',
+    urlAvatar: 'avatar-man.jpg',
+    urlProfile: '/comentarios_de_usuarios',
+    comment:
+      '”Depois que você começa a usar produtos de qualidade é bem difícil voltar... ”'
+  },
+  {
+    userName: 'Patricia Godoy',
+    city: 'São Paulo',
+    state: 'São Paulo',
+    urlUserBg: 'slide-2.jpg',
+    urlAvatar: 'avatar-woman.jpg',
+    urlProfile: '/comentarios_de_usuarios',
+    comment:
+      '”Não consigo mais viver sem os produtos do Sítio, a diferença do que eu comprava antes e agora é abissal”'
+  },
+  {
+    userName: 'Marcinho Ribeiro',
+    city: 'Belo Horizonte',
+    state: 'Minas Gerais',
+    urlUserBg: 'slide-1.jpg',
+    urlAvatar: 'avatar-man.jpg',
+    urlProfile: '/comentarios_de_usuarios',
+    comment:
+      '”Depois que você começa a usar produtos de qualidade é bem difícil voltar... ”'
+  }
+]
