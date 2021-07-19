142f394f2016d7c245dec306c7306b6c82f8492c
Guillermo Angulo <anyermo@gmail.com>
2021-07-19 06:08:18 -0300

    Add UsersComments to Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,9 @@
 import Welcome from 'components/Welcome'
+import UsersComments from 'components/UsersComments'
 import SocialComments from 'components/SocialComments'
 
 import mockWelcome from 'components/Welcome/mock'
+import mockUsersComments from 'components/UsersComments/mock'
 import mockSocialComments from 'components/SocialComments/mock'
 
 import * as S from './styles'
@@ -9,6 +11,10 @@ import * as S from './styles'
 const Main = () => (
   <S.Wrapper>
     <Welcome {...mockWelcome} />
+    <UsersComments
+      title="O que as pessoas estão achando"
+      items={mockUsersComments}
+    />
     <SocialComments
       title="O que estão falando nas redes sociais"
       comments={mockSocialComments}
