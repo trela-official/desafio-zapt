fc7626b04745c0bfa2789aac1e14a7d9eff4f2eb
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-18 23:14:59 -0300

    update manifest and _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,15 +9,9 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <title>Zapt</title>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092B" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-        />
+        <meta name="description" content="Desafio da Zapt" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
