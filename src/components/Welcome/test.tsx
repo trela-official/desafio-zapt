693d61bc05e708868da055fb35dd7514eaad675c
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-18 23:59:26 -0300

    improve welcome component test description

--- src/components/Welcome/test.tsx
+++ src/components/Welcome/test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from 'utils/test-utils'
 import Welcome from '.'
 
 describe('<Welcome />', () => {
-  it('should render the Welcome component', () => {
+  it('should render the Welcome component with the right username', () => {
     render(<Welcome username="Username" />)
 
     expect(screen.getByText('Bem vindo Username,')).toBeInTheDocument()
