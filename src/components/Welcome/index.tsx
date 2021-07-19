5930c2ab7467834d62bf2071219f58e7209a5bb9
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-18 23:57:19 -0300

    add actions

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -7,32 +7,42 @@ type WelcomePrps = {
   username: string
 }
 
-const Welcome = ({ username }: WelcomePrps) => (
-  <S.CardWrapper>
-    <S.ContentWrapper>
-      <S.BorderTopImage src="/img/orange-icon.png" />
+const Welcome = ({ username }: WelcomePrps) => {
+  const createGroup = () => {
+    window.alert('Criar um grupo')
+  }
 
-      <S.HeroWrapper>
-        <HeroCta
-          heroTitle={`Bem vindo ${username},`}
-          heroSubtitle="Para você aproveitar nossas oportunidades primeiro você precisa estar dentro de um grupo, estamos aqui para lhe ajudar."
-          ctaTitle="O que você prefere?"
-          firstOption={{ label: 'Entrar em um grupo' }}
-          secondOption={{ label: 'Criar um grupo' }}
-        />
-      </S.HeroWrapper>
+  const enterGroup = () => {
+    window.alert('Entrar em um grupo')
+  }
 
-      <S.WelcomeImageWrapper>
-        <S.Circle />
-        <S.Ellipse />
-      </S.WelcomeImageWrapper>
-      <S.BorderBottomImage src="/img/blue-icon.png" />
-    </S.ContentWrapper>
-    <SubtitleWithLink
-      text="Qualquer dúvida que você tiver é só nos chamar no nosso número de"
-      link={{ label: 'Whatsapp.', href: '/' }}
-    />
-  </S.CardWrapper>
-)
+  return (
+    <S.CardWrapper>
+      <S.ContentWrapper>
+        <S.BorderTopImage src="/img/orange-icon.png" />
+
+        <S.HeroWrapper>
+          <HeroCta
+            heroTitle={`Bem vindo ${username},`}
+            heroSubtitle="Para você aproveitar nossas oportunidades primeiro você precisa estar dentro de um grupo, estamos aqui para lhe ajudar."
+            ctaTitle="O que você prefere?"
+            firstOption={{ label: 'Entrar em um grupo', action: enterGroup }}
+            secondOption={{ label: 'Criar um grupo', action: createGroup }}
+          />
+        </S.HeroWrapper>
+
+        <S.WelcomeImageWrapper>
+          <S.Circle />
+          <S.Ellipse />
+        </S.WelcomeImageWrapper>
+        <S.BorderBottomImage src="/img/blue-icon.png" />
+      </S.ContentWrapper>
+      <SubtitleWithLink
+        text="Qualquer dúvida que você tiver é só nos chamar no nosso número de"
+        link={{ label: 'Whatsapp.', href: '/' }}
+      />
+    </S.CardWrapper>
+  )
+}
 
 export default Welcome
