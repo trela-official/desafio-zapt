ba7a7c776fb129eebe6f86417fbd64568f97fc16
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-07-19 04:13:45 -0300

    test SocialComments list

--- src/components/SocialComments/test.tsx
+++ src/components/SocialComments/test.tsx
@@ -1,13 +1,23 @@
 import { render, screen } from 'utils/test-utils'
+import comments from './mock'
 
 import SocialComments from '.'
 
+jest.mock('components/SocialCommentCard', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock SocialComment">{children}</div>
+  }
+}))
+
 describe('<SocialComments />', () => {
   it('should render the heading', () => {
-    const { container } = render(<SocialComments />)
+    render(<SocialComments comments={comments} />)
 
-    expect(screen.getByRole('heading', { name: /SocialComments/i })).toBeInTheDocument()
+    expect(
+      screen.getByText('O que estão falando nas redes sociais')
+    ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getAllByTestId('Mock SocialComment')).toHaveLength(9)
   })
 })
