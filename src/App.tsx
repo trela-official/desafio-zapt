e54c2c700b1d6e81516b6fd50a0e6ed65127bfc0
Peterson Pereira <petersonbozza7@gmail.com>
2021-07-18 21:53:25 -0300

    added persons`s photo

--- src/App.tsx
+++ src/App.tsx
@@ -12,44 +12,54 @@ function App() {
         <Column>
           <Review review={'Não da para parar de comer.. esse final de semana pedi 20 pasteis da Ilma e acabaram TODOS em um só dia... O pessoal aqui de casa ficou viciado de um nível que terei que comprar toda semana... Assim não tem academia que secura rsrsrsrs Obrigado Zapt.'} 
           name={'ferri-gui'}
+          imageSrc={'ferri-gui.png'}
           />
-
 
           <Review review={'Estava precisando achar algum lugar que vendia produtos que vêem direto da fazenda... a Zapt salvou.. e ainda paquei um preço muito mais barato que supermecado 🙌”'} 
           name={'paolaC'}
+          imageSrc={'paolaC.png'}
           />
 
           <Review review={'Todos os vinhos da vinícula Bueno estão disponíveis no catalogo da Zapt a um preço incrivél... não percam essa oportunidade...'} 
           name={'Galvão'}
+          imageSrc={'galvao.png'}
           />
 
         </Column>
 
         <Column>
           <Review review={'Nunca tinha achado os produtos na Lush depois que ela saiu do Brasil.. só a Zapt para conseguir importalos a preço mais barato do que vendiam aqui! Muito obrigado, recomendo sempre a todos os amigos.'}
-          name={'LiciaR'}
+          name={'LiciaR.'}
+          imageSrc={'liciaR.png'}
+
          />
 
           <Review review={'Ontem chegou uma pizza que comprei na Zapt e me lembrou as que minha avó fazia... sensacional!'}
           name={'shitz'}
+          imageSrc={'shitz.png'}
           />
 
           <Review review={'Nossa cada dia tem uma oferta mais legal do que a outra.. Além de produtos de qualidade o préco é imbatível”'}
           name={'rafael'}
+          imageSrc={'rafael.png'}
           />
+
       </Column>
 
       <Column>
         <Review review={'Achei uma promoção incrível de roupinha de bebê, foi o que salvou o enxoval do Pedro.'}
         name={'gust'}
+        imageSrc={'gust.png'}
         />
 
         <Review review={'Meu dono finalmente conseguiu achar uma comida que eu gosto! Agora vou conseguir engordar e ser feliz.'}
         name={'cookie'}
+        imageSrc={'cookie.png'}
         />
 
         <Review review={'Não conhecia a Zapt.. um amigo meu indicou... falou para eu dar uma olhada nos preços das cervejas artesanais.. incrível.'}
         name={'duque'}
+        imageSrc={'duque.png'}
         />
       </Column>
       </Main>
--- src/components/Review/index.tsx
+++ src/components/Review/index.tsx
@@ -3,13 +3,17 @@ import * as S from './styles'
 interface ReviewProps{
   review: string
   name: string
+  imageSrc: string
 }
 
-export function Review({review, name }: ReviewProps){
+export function Review({review, name, imageSrc }: ReviewProps){
   return(
     <S.Review>
       <S.TextReview>{review}</S.TextReview>
-      <S.Person>@{name}</S.Person>
+      <S.Person>
+        <img src={`peopleReview/${imageSrc}`} alt="Imagem da pessoa que avaliou a zapt." />
+        <S.Name>@{name}</S.Name>
+      </S.Person>
     </S.Review>
   )
 }
